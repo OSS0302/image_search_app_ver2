bcdb074a6969f36b8b2d827739c5e59b78e16216
Ohsangsoo <tkdtn3264@gmail.com>
2024-01-06 20:50:12 +0900

    이미지 데이터가 잘오는지 확인

---
+++ lib/data/data_source/image_api.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:image_search_app_ver2/key.dart';
+
+class ImageApi {
+  //  <Map<String,dynamic>> 을 한 이유는 DTO 을 안만들 수도 있기 떄문이 다, 의존하기 않기 때문데
+  Future <Map<String,dynamic>> getImageResult (String query) async {
+    final response =  await http.get(Uri.parse('https://pixabay.com/api/?key=$key=$query&image_type=photo'));
+    return jsonDecode(response.body);
+
+  }
+}
