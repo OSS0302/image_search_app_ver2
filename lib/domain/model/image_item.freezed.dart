87b992059567c39569e11fa579e41c49c141c568
OSS0302 <tkdtn3264@gmail.com>
2024-01-17 22:22:50 +0900

    클론코딩 01_17 클론

---
+++ lib/domain/model/image_item.freezed.dart
@@ -0,0 +1,197 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'image_item.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+ImageItem _$ImageItemFromJson(Map<String, dynamic> json) {
+  return _ImageItem.fromJson(json);
+}
+
+/// @nodoc
+mixin _$ImageItem {
+  String get imageUrl => throw _privateConstructorUsedError;
+  String get tags => throw _privateConstructorUsedError;
+  int get id => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ImageItemCopyWith<ImageItem> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ImageItemCopyWith<$Res> {
+  factory $ImageItemCopyWith(ImageItem value, $Res Function(ImageItem) then) =
+      _$ImageItemCopyWithImpl<$Res, ImageItem>;
+  @useResult
+  $Res call({String imageUrl, String tags, int id});
+}
+
+/// @nodoc
+class _$ImageItemCopyWithImpl<$Res, $Val extends ImageItem>
+    implements $ImageItemCopyWith<$Res> {
+  _$ImageItemCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? imageUrl = null,
+    Object? tags = null,
+    Object? id = null,
+  }) {
+    return _then(_value.copyWith(
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      tags: null == tags
+          ? _value.tags
+          : tags // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ImageItemImplCopyWith<$Res>
+    implements $ImageItemCopyWith<$Res> {
+  factory _$$ImageItemImplCopyWith(
+          _$ImageItemImpl value, $Res Function(_$ImageItemImpl) then) =
+      __$$ImageItemImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String imageUrl, String tags, int id});
+}
+
+/// @nodoc
+class __$$ImageItemImplCopyWithImpl<$Res>
+    extends _$ImageItemCopyWithImpl<$Res, _$ImageItemImpl>
+    implements _$$ImageItemImplCopyWith<$Res> {
+  __$$ImageItemImplCopyWithImpl(
+      _$ImageItemImpl _value, $Res Function(_$ImageItemImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? imageUrl = null,
+    Object? tags = null,
+    Object? id = null,
+  }) {
+    return _then(_$ImageItemImpl(
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      tags: null == tags
+          ? _value.tags
+          : tags // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ImageItemImpl with DiagnosticableTreeMixin implements _ImageItem {
+  const _$ImageItemImpl(
+      {required this.imageUrl, required this.tags, required this.id});
+
+  factory _$ImageItemImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ImageItemImplFromJson(json);
+
+  @override
+  final String imageUrl;
+  @override
+  final String tags;
+  @override
+  final int id;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'ImageItem(imageUrl: $imageUrl, tags: $tags, id: $id)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'ImageItem'))
+      ..add(DiagnosticsProperty('imageUrl', imageUrl))
+      ..add(DiagnosticsProperty('tags', tags))
+      ..add(DiagnosticsProperty('id', id));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ImageItemImpl &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            (identical(other.tags, tags) || other.tags == tags) &&
+            (identical(other.id, id) || other.id == id));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, imageUrl, tags, id);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ImageItemImplCopyWith<_$ImageItemImpl> get copyWith =>
+      __$$ImageItemImplCopyWithImpl<_$ImageItemImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ImageItemImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _ImageItem implements ImageItem {
+  const factory _ImageItem(
+      {required final String imageUrl,
+      required final String tags,
+      required final int id}) = _$ImageItemImpl;
+
+  factory _ImageItem.fromJson(Map<String, dynamic> json) =
+      _$ImageItemImpl.fromJson;
+
+  @override
+  String get imageUrl;
+  @override
+  String get tags;
+  @override
+  int get id;
+  @override
+  @JsonKey(ignore: true)
+  _$$ImageItemImplCopyWith<_$ImageItemImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/domain/model/image_item.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'image_item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ImageItemImpl _$$ImageItemImplFromJson(Map<String, dynamic> json) =>
+    _$ImageItemImpl(
+      imageUrl: json['imageUrl'] as String,
+      tags: json['tags'] as String,
+      id: json['id'] as int,
+    );
+
+Map<String, dynamic> _$$ImageItemImplToJson(_$ImageItemImpl instance) =>
+    <String, dynamic>{
+      'imageUrl': instance.imageUrl,
+      'tags': instance.tags,
+      'id': instance.id,
+    };
