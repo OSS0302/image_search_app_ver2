24e03b27c2f411804b59918aebf1e344315d0de6
OSS0302 <tkdtn3264@gmail.com>
2024-01-18 20:58:18 +0900

    모델 클래스 작성

--- lib/domain/model/image_item.dart
+++ lib/domain/model/image_item.dart
@@ -1,17 +1,63 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:flutter/foundation.dart';
+class ImageItem {
+  String imageUrl;
+  String tage;
+  int id;
 
-part 'image_item.freezed.dart';
+//<editor-fold desc="Data Methods">
+  ImageItem({
+    required this.imageUrl,
+    required this.tage,
+    required this.id,
+  });
 
-part 'image_item.g.dart';
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is ImageItem &&
+          runtimeType == other.runtimeType &&
+          imageUrl == other.imageUrl &&
+          tage == other.tage &&
+          id == other.id);
 
-@freezed
-class ImageItem with _$ImageItem {
-  const factory ImageItem({
-    required String imageUrl,
-    required String tags,
-    required int id,
-  }) = _ImageItem;
+  @override
+  int get hashCode => imageUrl.hashCode ^ tage.hashCode ^ id.hashCode;
 
-  factory ImageItem.fromJson(Map<String, Object?> json) => _$ImageItemFromJson(json);
+  @override
+  String toString() {
+    return 'ImageItem{' +
+        ' imageUrl: $imageUrl,' +
+        ' tage: $tage,' +
+        ' id: $id,' +
+        '}';
+  }
+
+  ImageItem copyWith({
+    String? imageUrl,
+    String? tage,
+    int? id,
+  }) {
+    return ImageItem(
+      imageUrl: imageUrl ?? this.imageUrl,
+      tage: tage ?? this.tage,
+      id: id ?? this.id,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'imageUrl': this.imageUrl,
+      'tage': this.tage,
+      'id': this.id,
+    };
+  }
+
+  factory ImageItem.fromJson(Map<String, dynamic> json) {
+    return ImageItem(
+      imageUrl: json['imageUrl'] as String,
+      tage: json['tage'] as String,
+      id: json['id'] as int,
+    );
+  }
+
+//</editor-fold>
 }
