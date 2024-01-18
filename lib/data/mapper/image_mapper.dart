f6acdddd707849ded1b891cffbc5cd4ce95f8c74
OSS0302 <tkdtn3264@gmail.com>
2024-01-18 20:58:40 +0900

    매퍼 작성

--- lib/data/mapper/image_mapper.dart
+++ lib/data/mapper/image_mapper.dart
@@ -1,12 +1,8 @@
-import 'package:image_search_app_ver2/data/dto/image_dto.dart';
+import 'package:image_search_app_ver2/data/dto/pixabay_dto.dart';
 import 'package:image_search_app_ver2/domain/model/image_item.dart';
 
-extension toImageModel on Hits {
-  ImageItem toImageItem() {
-    return ImageItem(
-      imageUrl: previewURL ?? '',
-      tags: tags ?? '',
-      id: id as int,
-    );
+extension ToModel on Hits {
+  ImageItem toImageItem(){
+    return ImageItem(imageUrl: previewURL ?? '', tage: tags ?? '', id: id as int);
   }
 }
